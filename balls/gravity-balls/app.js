9a9ed6f9306c74d2eb939339d8634f714fe6f822
Vsevolod Brovarnyi <vsevolod.brovarnyi@synapse.com>
2018-01-25 14:37:15 +0100

    gravity finish

--- balls/gravity-balls/app.js
+++ balls/gravity-balls/app.js
@@ -5,6 +5,10 @@ canvas.width = innerWidth;
 
 c = canvas.getContext('2d');
 
+canvas.addEventListener('click', function() {
+    init();    
+})
+
 var ballArray = [],
     colorArray = ['#3D9970', '#39CCCC', '#FF4136', '#85144b', '#FF851B'],
     gravity = 0.2,
@@ -14,11 +18,12 @@ function randomPosition(min, max) {
     return Math.floor((Math.random() * (max - min + 1) + min));
 }
 
-function Ball(x, y, radius, color, dy) {
+function Ball(x, y, radius, color, dx, dy) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.dx = dx;
     this.dy = dy;
 
     this.draw = function () {
@@ -32,20 +37,29 @@ function Ball(x, y, radius, color, dy) {
     this.update = function() {   
         if(this.y + this.radius + this.dy >= innerHeight) {
             this.dy = -this.dy;
-			this.dy = this.dy * friction;
+            this.dy = this.dy * friction;
+            this.dx = this.dx*friction;
         } else this.dy += gravity;
+        if(this.x + this.radius + this.dx >=innerWidth || this.x - this.radius <=0) {
+            this.dx = -this.dx*friction;
+        }
+        this.x += this.dx;  
         this.y += this.dy;  
         this.draw();      
     }
 }
 
-for(var i = 0; i < 100; i++) {
-    ballArray.push(new Ball(randomPosition(20, innerWidth-20),
-                            randomPosition(20, innerHeight-20),
-                            20, 
-                            colorArray[randomPosition(0, colorArray.length-1)],
-                            3))   
-                            ballArray[i].update();
+function init() {    
+    ballArray = [];
+    for(var i = 0; i < 100; i++) {
+        ballArray.push(new Ball(randomPosition(20, innerWidth-20),
+                                randomPosition(20, innerHeight-20),
+                                20, 
+                                colorArray[randomPosition(0, colorArray.length-1)],
+                                randomPosition(1, 3),
+                                randomPosition(2, 5)))   
+                                ballArray[i].update();
+    }
 }
 
 function animation() {
@@ -55,6 +69,7 @@ function animation() {
         ballArray[i].update();
     }
 }
+init();
 animation();
 
 
